9125ba0329bbc6a76a0ccdf11faaf96d7e1826ec
GroupBy <groupby@C02WN1F3G8WN.local>
2018-06-13 08:39:38 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Riot todo</title>
+    <script src="https://cdn.jsdelivr.net/riot/2.3/riot+compiler.min.js"></script>
+  </head>
+  <body>
+    <!-- Tags added here -->
+    <!-- <todo></todo> -->
+
+    <!-- Links to the tags here -->
+    <!-- <script src="tags/todo.tag" type="riot/tag"></script> -->
+
+    <!-- Mount tags with riot.mount() -->
+    <script>
+        //riot.mount('todo')
+    </script>
+  </body>
+</html>
